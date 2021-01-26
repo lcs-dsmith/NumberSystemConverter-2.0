3535cbfbee1dee561fab49e306f76cdcaadb29ac
lcs-dsmith <dsmith@lcs.on.ca>
2021-01-26 14:59:39 -0500

    Got the initial prompt done

--- NumberSystemConverter/main.swift
+++ NumberSystemConverter/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+// Input
+
+// Get the "from" number system
+print("What number system do you want to convert from?")
+print("B: Binary")
+print("O: Octal")
+print("D: Decimal")
+print("H: Hexadecimal")
+print("Enter your choice (B/O/D/H : ", terminator: "")
+let from = readLine()!
+// Process
+print("From")
+
+
+
+
+// Output
 
